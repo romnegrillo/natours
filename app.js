b9f52149ea6f4763262a75c26f5bbb24e19be437
Romulo Negrillo Jr <romnegrillo@gmail.com>
2023-03-08 15:40:47 +0800

    Connected global error handle in all tour controllers.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+console.log("DEBUG");
+console.log(globalErrorHandler);
+
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -29,21 +35,9 @@ app.all("*", (req, res, next) => {
   //   message: `Can't find ${req.url} on this server!`,
   // });
 
-  const err = new Error(`Can't find ${req.url} on this server`);
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.url} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
