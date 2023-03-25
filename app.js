6f64544e246ba468a9dc3e71fe9b820c6a245f61
Romulo Negrillo Jr <romnegrillo@gmail.com>
2023-03-26 01:13:10 +0800

    Added helmet package. Refactored app.js to explain the middleware stack.

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
-
-const AppError = require("./utils/appError");
+const helmet = require("helmet");
 const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
-console.log(globalErrorHandler);
-
+// Import routes.
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
+// App instance.
 const app = express();
+
+// Global middlewares.
 app.use(express.static(`${__dirname}/dev-data/`));
 
+// 1.) Helmet for setting https headers.
+app.use(helmet());
+
+// 2.) Morgan for logging requests.
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// 3.) Rate limiting.
 const limiter = rateLimit({
   max: 10,
   windowMs: 60 * 60 * 100, // in milliseconds
   message: "Too many requests from this IP, please try again in an hour.",
 });
 app.use("/api/v1", limiter);
+
+// 4.) Test middleware.
+app.use((req, res, next) => {
+  console.log("Hello from middleware.");
+  next();
+});
 
 // Routers.
 app.use("/api/v1/tours", tourRouter);
